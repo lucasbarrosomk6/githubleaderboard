bca2460b46cd19a851e81a505c97790b4dca2f86
Cristian Florea <florecra@novartis.com>
2020-02-16 21:04:01 +0100

    mentoring session

--- src/App.js
+++ src/App.js
@@ -1,79 +1,79 @@
 import React from "react";
+import axios from "axios";
 import "./App.css";
 import Leader from "./components/leader/leader.component.jsx";
 import Leaderboard from "./components/leaderboard/leaderboard.component.jsx";
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      searchField: "",
-      unsortedArray: []
-    };
-    this.fetchUser = this.fetchUser.bind(this);
-    this.handleChange = this.handleChange.bind(this);
-  }
+  state = {
+    searchField: "",
+    users: []
+  };
 
-  handleChange(e) {
-    e.preventDefault();
+  handleChange = e => {
     this.setState({ searchField: e.target.value });
-  }
+  };
 
-  fetchUser() {
-    fetch(`https://api.github.com/users/${this.state.searchField}`)
-      .then(data => data.json())
-      .then(data => {
-        this.setState(prevState => ({
-          unsortedArray: prevState.unsortedArray.concat(data)
-        }));
-      })
-      .catch(error => {
-        console.log(error, "something happend");
-      }); //fetches the username data based on the username variable above and set it to state
-    console.log(this.state.unsortedArray);
-  }
+  fetchUser = async e => {
+    try {
+      e.preventDefault();
+      this.setState({ error: false });
+      const { searchField, users } = this.state;
+      const query = searchField.trim();
+
+      if (users.find(user => user.login === query) || !query) {
+        this.setState({
+          searchField: "",
+          error: true
+        });
+        return;
+      }
+
+      this.setState({ loading: true });
+
+      const { data } = await axios(`https://api.github.com/users/${query}`);
+
+      this.setState(({ users }) => ({
+        users: [...users, data],
+        loading: false,
+        searchField: ""
+      }));
+    } catch (err) {
+      this.setState({ error: true, loading: false });
+    }
+  };
+
   render() {
-    const sortedArray = this.state.unsortedArray.sort(
+    const { users, error, loading } = this.state;
+    const sortedUsers = users.sort(
       (a, b) => b.public_repos + b.followers - (a.public_repos + a.followers)
     );
-    if (this.state.unsortedArray.length)
-      return (
-        <div className="App">
-          <div className="header">
-            <input
-              type="text"
-              value={this.state.searchField}
-              onChange={this.handleChange}
-            />
-            <button onClick={this.fetchUser}>fetch</button>
+
+    return (
+      <div className="App">
+        <form
+          className="header"
+          style={{ borderColor: error && "red" }}
+          onSubmit={this.fetchUser}
+        >
+          <input
+            type="text"
+            value={this.state.searchField}
+            onChange={this.handleChange}
+          />
+          <button>fetch</button>
+        </form>
+        {loading && <div>loading</div>}
+        <div className="main">
+          <div className="left">
+            <Leader leader={sortedUsers[0]} />
           </div>
-          <div className="main">
-            <div className="left">
-              <Leader leader={sortedArray[0]} />
-            </div>
-            <div className="right">
-              <Leaderboard players={sortedArray} />
-            </div>
-          </div>
-        </div>
-      );
-    if (!this.state.unsortedArray.length)
-      return (
-        <div className="App">
-          <div className="header">
-            <input
-              type="text"
-              value={this.state.searchField}
-              onChange={this.handleChange}
-            />
-            <button onClick={this.fetchUser}>fetch</button>
-          </div>
-          <div className="main">
-            <div className="left"><Leader/></div>
-            <div className="right"><Leaderboard/></div>
+          <div className="right">
+            <Leaderboard players={sortedUsers} />
           </div>
         </div>
-      );
+      </div>
+    );
   }
 }
 
--- src/components/leaderboard/leaderboard.component.jsx
+++ src/components/leaderboard/leaderboard.component.jsx
@@ -2,38 +2,33 @@ import React from "react";
 import "./leaderboard.styles.css";
 import PlayerCard from "./player-card/player-card.component";
 
+const defaults = [
+  { login: "example 1", public_repos: 0, followers: 0 },
+  { login: "example 1", public_repos: 0, followers: 0 },
+  { login: "example 1", public_repos: 0, followers: 0 }
+];
+
 const Leaderboard = props => (
   <div className="leaderboard-card">
     <PlayerCard place="" name="name" score="score" />
 
-    {
-    props.players?props.players.map((player, index) => (
-      <PlayerCard
-        key={player.id}
-        place={index + 1}
-        name={player.login}
-        score={player.public_repos + player.followers}
-      />
-    )):
-    <div className="no-data-display">
-       <PlayerCard
-        place="1"
-        name= "example 1"
-        score="20"
-      />
-      <PlayerCard
-      place="2"
-      name= "example 2"
-      score="10"
-    />
-    <PlayerCard
-    place="3"
-    name= "example 3"
-    score="4"
-  />
-    </div>
-   
-    }
+    {props.players.length
+      ? props.players.map((player, index) => (
+          <PlayerCard
+            key={player.id}
+            place={index + 1}
+            name={player.login}
+            score={player.public_repos + player.followers}
+          />
+        ))
+      : defaults.map((player, index) => (
+          <PlayerCard
+            key={player.id}
+            place={index + 1}
+            name={player.login}
+            score={player.public_repos + player.followers}
+          />
+        ))}
   </div>
 );
 
